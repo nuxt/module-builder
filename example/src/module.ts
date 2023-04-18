386ffa121fb31d0ad2ce503012a9d5d7ceab4138
Daniel Roe <daniel@roe.dev>
2023-04-18 11:41:11 +0100

    chore: update fixture

--- example/src/module.ts
+++ example/src/module.ts
@@ -1,5 +1,6 @@
-import { defineNuxtModule } from '@nuxt/kit'
+import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
 
+// Module options TypeScript interface definition
 export interface ModuleOptions {
   apiKey: string
 }
@@ -21,8 +22,14 @@ export default defineNuxtModule<ModuleOptions>({
     name: 'my-module',
     configKey: 'myModule'
   },
+  // Default configuration options of the Nuxt module
   defaults: {
     apiKey: ''
   },
-  setup (_options, _nuxt) {}
+  setup (_options, _nuxt) {
+    const resolver = createResolver(import.meta.url)
+
+    // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
+    addPlugin(resolver.resolve('./runtime/plugin'))
+  }
 })
--- example/src/runtime/plugin.ts
+++ example/src/runtime/plugin.ts
@@ -1,7 +1,6 @@
-// @ts-ignore
 import { defineNuxtPlugin } from '#app'
 
 export default defineNuxtPlugin((_nuxtApp) => {
   // eslint-disable-next-line no-console
-  console.log('Plugin by my-module!')
+  console.log('Plugin injected by my-module!')
 })
