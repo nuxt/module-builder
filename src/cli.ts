71bb35175496929bfe52eef03d84c7678504cd93
Daniel Roe <daniel@roe.dev>
2023-05-16 15:03:54 +0000

    feat: add `--prepare` mode for local types support

--- src/cli.ts
+++ src/cli.ts
@@ -5,13 +5,49 @@ import mri from 'mri'
 import { resolve } from 'pathe'
 import { buildModule } from './build'
 
+// TODO: use citty
 async function main () {
+  const args = mri(process.argv.slice(2))
+  if ('prepare' in args) {
+    return prepare()
+  }
+  return build()
+}
+
+async function build () {
   const args = mri(process.argv.slice(2))
   const rootDir = resolve(args._[0] || '.')
   await buildModule({
     rootDir,
     outDir: args.outDir,
     stub: args.stub
+  })
+}
+
+async function prepare () {
+  const { runCommand } = await import('nuxi')
+  const args = mri(process.argv.slice(2))
+  const rootDir = resolve(args._[0] || '.')
+
+  return runCommand('prepare', [rootDir], {
+    overrides: {
+      typescript: {
+        builder: 'shared'
+      },
+      imports: {
+        autoImport: false
+      },
+      modules: [
+        resolve(rootDir, './src/module'),
+        function (options, nuxt) {
+          nuxt.hooks.hook('app:templates', app => {
+            for (const template of app.templates) {
+              template.write = true
+            }
+          })
+        }
+      ]
+    }
   })
 }
 
