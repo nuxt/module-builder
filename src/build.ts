7da7ba1ca7c7206f4afefea00c3ffd9a2f41fee6
Bobbie Goede <bobbiegoede@gmail.com>
2023-08-31 14:48:53 +0200

    feat: support `--sourcemap` option (#163)

--- src/build.ts
+++ src/build.ts
@@ -7,6 +7,7 @@ import { findExports } from 'mlly'
 
 export interface BuildModuleOptions {
   rootDir: string
+  sourcemap?: boolean
   stub?: boolean
   outDir?: string
 }
@@ -18,6 +19,7 @@ export async function buildModule (opts: BuildModuleOptions) {
 
   await build(opts.rootDir, false, {
     declaration: true,
+    sourcemap: opts.sourcemap,
     stub: opts.stub,
     outDir,
     entries: [
