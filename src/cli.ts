08666c1509f83df90cca341049f0d9b3bf581f30
Daniel Roe <daniel@roe.dev>
2023-09-28 09:41:06 +0100

    fix: deprecate implicit build usage

--- src/cli.ts
+++ src/cli.ts
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 import { defineCommand, runMain } from 'citty'
 import type { CommandDef } from 'citty'
+import { consola } from 'consola'
 import { name, description, version } from '../package.json'
 
 const _rDefault = (r: any) => (r.default || r) as Promise<CommandDef>
@@ -19,6 +20,7 @@ const main = defineCommand({
     // TODO: support 'default command' in citty?
     const firstArg = context.rawArgs[0]
     if (!(firstArg in context.cmd.subCommands)) {
+      consola.warn('Please specify the `build` command explicitly. In a future version of `@nuxt/module-builder`, the implicit default build command will be removed.')
       context.rawArgs.unshift('build')
     }
   }
