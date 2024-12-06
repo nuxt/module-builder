6d31dfb4509656222a465a14d6cb67b2dcb857b7
Daniel Roe <daniel@roe.dev>
2024-12-06 12:53:32 +0000

    chore: use script for type testing

---
+++ example/playground/scripts/prepare.mjs
@@ -0,0 +1,15 @@
+import { writeFileSync } from 'fs'
+import { execSync } from 'child_process'
+
+const MODULE_RESOLUTION = process.env.MODULE_RESOLUTION
+
+if (!MODULE_RESOLUTION) {
+  throw new Error('MODULE_RESOLUTION should be defined')
+}
+
+// Write the .nuxtrc file
+writeFileSync('.nuxtrc', `typescript.tsConfig.compilerOptions.moduleResolution=${MODULE_RESOLUTION}\n`)
+
+// Run the commands
+execSync('nuxt prepare', { stdio: 'inherit' })
+execSync('vue-tsc --noEmit', { stdio: 'inherit' })
