d1aed739e738ecd08436fa55333e2b5ae371e6a9
Daniel Roe <daniel@roe.dev>
2025-01-06 14:04:03 +0000

    feat(build): ignore test + story files in `runtime/` directory

--- src/commands/build.ts
+++ src/commands/build.ts
@@ -62,6 +62,11 @@ export default defineCommand({
           outDir: `${outDir}/runtime`,
           addRelativeDeclarationExtensions: true,
           ext: 'js',
+          pattern: [
+            '**',
+            '!**/*.stories.{js,cts,mts,ts,jsx,tsx}', // ignore storybook files
+            '!**/*.{spec,test}.{js,cts,mts,ts,jsx,tsx}', // ignore tests
+          ],
           esbuild: {
             jsxImportSource: 'vue',
             jsx: 'automatic',
