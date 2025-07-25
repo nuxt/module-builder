34ad4bb8378c3dc1da0449c10e9b7cbd2d30ee1f
Daniel Roe <daniel@roe.dev>
2025-07-25 10:34:37 +0100

    chore: update snapshots

--- test/__snapshots__/TestMeSetup.vue.d.ts
+++ test/__snapshots__/TestMeSetup.vue.d.ts
@@ -1,5 +1,5 @@
 type __VLS_Props = {
     count: number;
 };
-declare const _default: import("@vue/runtime-core").DefineComponent<__VLS_Props, {}, {}, {}, {}, import("@vue/runtime-core").ComponentOptionsMixin, import("@vue/runtime-core").ComponentOptionsMixin, {}, string, import("@vue/runtime-core").PublicProps, Readonly<__VLS_Props> & Readonly<{}>, {}, {}, {}, {}, string, import("@vue/runtime-core").ComponentProvideOptions, false, {}, any>;
+declare const _default: import("@vue/runtime-core").DefineComponent<__VLS_Props, void, {}, {}, {}, import("@vue/runtime-core").ComponentOptionsMixin, import("@vue/runtime-core").ComponentOptionsMixin, {}, string, import("@vue/runtime-core").PublicProps, Readonly<__VLS_Props> & Readonly<{}>, {}, {}, {}, {}, string, import("@vue/runtime-core").ComponentProvideOptions, false, {}, any>;
 export default _default;
