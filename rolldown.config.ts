b8dd9cf2ce6f4720dc8a0cf3e4a39a1203399310
Damian Glowala <damian.glowala.rebkow@gmail.com>
2026-05-01 11:14:23 +0200

    update external and input rolldown opts

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -1,23 +1,18 @@
 import { defineConfig } from 'rolldown'
 import { dts } from 'rolldown-plugin-dts'
-import { globSync } from 'tinyglobby'
-import path from 'pathe'
 
 export default defineConfig({
-  input: Object.fromEntries(
-    globSync('src/**/*.ts').map(file => [
-      path.relative('src', file.slice(0, file.length - path.extname(file).length))
-        .split(path.sep)
-        .join('/'),
-      path.resolve(file),
-    ]),
-  ),
+  input: [
+    './src/cli.ts',
+    './src/index.ts',
+  ],
   platform: 'node',
-  external: /^[^./]/,
+  external: /^(?!\.)/,
   plugins: [
     dts(),
   ],
   output: {
     entryFileNames: '[name].mjs',
+    chunkFileNames: '[name].mjs',
   },
 })
